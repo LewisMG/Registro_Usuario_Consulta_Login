3be0f58ce528fe1433917597b94b5ed77639669b
Lewis Mendoza <Lewis_mendoza@ucne.edu.do>
2019-01-25 23:50:29 -0400

    Creada la entidad usuario con sus propiedades y la clase contexto

--- Entities/Usuario.cs
+++ Entities/Usuario.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,5 +9,22 @@ namespace Entities
 {
     public class Usuario
     {
+        [Key]
+        public int UsuarioId { get; set; }
+        public DateTime Fecha { get; set; }
+        public string Nombre { get; set; }
+        public string NombreUser { get; set; }
+        public string Contraseña { get; set; }
+        public string vContraseña { get; set; }
+        public string Email { get; set; }
+        public int Telefono { get; set; }
+        public int celular { get; set; }
+
+        public Usuario()
+        {
+
+        }
     }
+
 }
+
